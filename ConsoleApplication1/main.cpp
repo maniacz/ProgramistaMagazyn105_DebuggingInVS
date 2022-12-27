45f656100af1f23ae14b639ffaa29a1279d73246
Lukasz Kipka <lukasz.kipka@pb.com>
2022-12-27 14:33:46 +0100

    Add project files.

---
+++ ConsoleApplication1/main.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include <stdio.h>
+
+struct Test
+{
+    unsigned char tab[4];
+    int value;
+};
+
+int main()
+{
+    Test test;
+    test.value = 0x11223344;
+
+    for (int i = 0; i <= 4; i++)
+    {
+        test.tab[i] = 0xAA;
+    }
+
+    printf("%x", test.value);
+    getchar();
+}
